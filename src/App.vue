6f55f27fda34262bedbbf19ac7be809d0690860c
dupeng <dupeng@mxnavi.com>
2022-06-28 13:59:09 +0800

    '上传代码'

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { watchOnce  } from '@vueuse/core'
+import {ref} from 'vue'
+
+const source = ref(0)
+
+watchOnce(
+  source,
+  (value) => {
+    console.log('变化了', value)
+  },
+)
+
+const clickedFn = () => {
+  source.value ++
+}
+
+</script>
+
+<template>
+ <div>{{source}}</div>
+  <button @click="clickedFn">
+    点击按钮
+  </button>
+</template>
