0800306bf1a021173289e07d6e2f949d2959f57f
dupeng <dupeng@mxnavi.com>
2022-06-29 11:04:44 +0800

    增加手动实现watchOnce功能的示例代码

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { watchOnce  } from '@vueuse/core'
 import {ref} from 'vue'
 
@@ -8,6 +8,27 @@ watchOnce(
   source,
   (value) => {
     console.log('变化了', value)
+  },
+)
+
+const clickedFn = () => {
+  source.value ++
+}
+
+</script> -->
+<script setup lang="ts">
+import {ref, watch} from 'vue'
+
+const source = ref(0)
+const count = ref(0)
+
+watch(
+  source,
+  (value) => {
+    if (count.value === 0) {
+      console.log('变化了', value)
+      count.value++
+    }
   },
 )
 
